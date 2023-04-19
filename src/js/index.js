89bcc55584f1e50552205e6c410209ffcaf0c19d
ruskTH <rusk2019@163.com>
2023-04-19 20:40:24 +0800

    安装css-loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
+import '../css/index.css'
+
+
 console.log(12);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
+const path = require("path");//node模块的函数
 
 module.exports = {
   entry: "./src/js/index.js", //设置入口
-  output: {
-    path: path.resolve(__dirname, "./dist"), //设置出口
+  output: {//设置出口
+    path: path.resolve(__dirname, "./dist"), //node模块的函数path.resolve
     filename: "bundle.js",//设置打包后的文件名
   },
 };
