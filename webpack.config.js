19f41d9d50fe9b9e63663a18bd100c040eca2b0a
ruskTH <rusk2019@163.com>
2023-04-25 00:28:34 +0800

    配置cssAndstyleLoader发现test的值不能加引号

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,24 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"), //node模块的函数path.resolve
     filename: "bundle.js",//设置打包后的文件名
   },
+
+  module:{
+    rules:[
+      {
+        test:/\.css$/,//  \=转译只匹配. .不转译可被匹配到其他字符 $=文件结尾    
+        // loader:'css-loader'//loader语法糖写法
+        use:[//完整写法
+        // {loader:'css-loader',opticon:''//可传递额外的参数}//一般直接如下面写即可
+        //loader执行顺序从下往上
+        'style-loader',
+        'css-loader'
+        ]
+      }
+    // {
+    //   test: /\.css$/i,
+    //   use: ["style-loader", "css-loader"],
+    // },
+
+    ]
+  }
 };
